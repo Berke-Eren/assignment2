f2b578ae84a20fb6f46612c6fe0643c0f2a14a65
348736794 <348736794@C-G3XSJB2.tdsb.on.ca>
2025-05-13 11:30:52 -0400

    added article classes

---
+++ src/FakeExpertArticle.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import NewsArticle;
+
+/**
+ *
+ * @author 348736794
+ */
+public class FakeExpertArticle extends NewsArticle {
+    private String explanation = "This article is based solely on the words of an \"expert\" who is not credible.";
+    
+    public FakeExpertArticle(String headline, String content, boolean validity, String additionalInformation){
+        super(headline, content, validity, additionalInformation);
+    }
+    
+    public FakeExpertArticle(String headline, String content, boolean validity){
+        super(headline, content, validity);
+    }
+    @Override
+    public String getExplanation(){
+        return explanation;
+    }
+}
---
+++ src/LackingFactsArticle.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import NewsArticle;
+
+/**
+ *
+ * @author 348736794
+ */
+public class LackingFactsArticle extends NewsArticle {
+    private String explanation = "This article tries to pass off the author's opinion as fact. There is no evidence to support the content of the article.";
+    
+    public LackingFactsArticle(String headline, String content, boolean validity, String additionalInformation){
+        super(headline, content, validity, additionalInformation);
+    }
+    
+    public LackingFactsArticle(String headline, String content, boolean validity){
+        super(headline, content, validity);
+    }
+    
+    public String getExplanation(){
+        return explanation;
+    }
+
+}
